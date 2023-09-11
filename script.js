a2c8e24661167dcec61133a9bbae96b68d16435d
akanshavrma <134785932+akansha2222@users.noreply.github.com>
2023-09-11 12:36:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ // Write your code her
+const charFrequency = {};
+	for(let char Of str)
+		{
+			charFrequency[char] = charFrequency[char] + 1 || 1; 
+			
+		}
+		for(let char Of str)
+		{
+			if(charFrequency[char] === 1){
+				return char;
+			}
+			
+		}
+	return null;
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
